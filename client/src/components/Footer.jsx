dad133b54087c0d06c05041c96722c7241ae7c4b
Arnau Calderón <arncalderon@gmail.com>
2024-08-30 11:05:15 +0200

    Add basic footer structure

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Registra un restaurante</a>
+            </li>
+            <li>
+              <a href="#">Trabaja con nosotros</a>
+            </li>
+            <li>
+              <a href="#">Política de privacidad</a>
+            </li>
+            <li>
+              <a href="#">Condiciones del servicio</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+}
