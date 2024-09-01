1b86138859fb04a3ec05312559579b92b0e6e461
mariaarce <mariavarcenegui@gmail.com>
2024-09-01 14:11:58 +0200

    review

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from '../src/components/templates/Login';
-import Register from '../src/components/templates/Register';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../src/components/templates/Login";
+import Register from "../src/components/templates/Register";
+import Review from "../src/components/templates/Review";
+import ReviewFront from "./pages/restaurant.js";
 
 const App = () => {
   return (
     <Router>
+      <ReviewFront />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </Router>
   );
--- server/src/api/models/user.model.js
+++ server/src/api/models/user.model.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-
+const userSchema = new Schema(
+  {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     picture: { type: String },
-    role: { type: String, required: true, enum: ["user", "owner"], default: "user" },
-
-
-}, {
-    collection: "users"
-});
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "owner"],
+      default: "user",
+    },
+  },
+  {
+    collection: "users",
+  }
+);
 
 const User = mongoose.model("users", userSchema);
 
