db96f31fcf03e6cfeff5d5421a61679688276c62
Arnau Calderón <arncalderon@gmail.com>
2024-09-02 12:08:09 +0200

    Add correct version to App.old.js

--- client/src/App.old.js
+++ client/src/App.old.js
@@ -1,11 +1,24 @@
 import React from "react";
-import AppRoutes from "../src/routes/Routes.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../src/components/templates/Login";
+import Register from "../src/components/templates/Register";
+import Review from "./components/cards/reviewCard.jsx";
+import ReviewFront from "./pages/reviewPage.js";
+import ReservationCard from "./components/cards/reservationCard.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
   return (
-    <div>
-      <AppRoutes />
-    </div>
+    <Router>
+      <ReviewFront />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/reservation" element={<ReservationCard />} />
+      </Routes>
+    </Router>
   );
 };
 
