91d142aa280dd7502822e3c958cd739092c79641
Clara <claraolivam96@gmail.com>
2024-09-02 21:23:57 +0200

    ruta 404

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -6,7 +6,7 @@ import Review from "../components/cards/reviewCard.jsx";
 import ReviewFront from "../pages/reviewPage.js";
 import ReservationCard from "../components/cards/reservationCard.jsx";
 import Home from "../pages/Home.jsx";
-import NavBar from "../components/templates/navBar.jsx";
+import NavBar from "../components/templates/Navbar.jsx";
 import Footer from "../components/templates/Footer.jsx";
 import RestaurantList from "../components/templates/RestaurantList.jsx";
 import RestaurantCard from "../components/cards/restaurantCard.jsx";
@@ -14,6 +14,7 @@ import RestaurantPage from "../pages/RestaurantPage.jsx";
 import ThemeProvider from "../context/ThemeContext";
 import Profile from "../pages/Profile.jsx";
 import ReservationForm from "../pages/ReservationForm.jsx";
+import NotFound from "../components/templates/NotFound.jsx";
 
 const AppRoutes = () => {
   return (
@@ -33,6 +34,7 @@ const AppRoutes = () => {
           <Route path="/restaurant" element={<RestaurantPage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/reservations/:id?" element={<ReservationForm />} />{" "}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </ThemeProvider>
