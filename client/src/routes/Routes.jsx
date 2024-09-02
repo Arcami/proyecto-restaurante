94abe8d0e0d9c4b48beb41de10486d2efb96fbe0
Arnau Calderón <arncalderon@gmail.com>
2024-09-02 22:04:06 +0200

    Add protected routes

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../components/templates/Login.jsx";
 import Register from "../components/templates/Register.jsx";
-import Review from "../components/cards/reviewCard.jsx";
-import ReviewFront from "../pages/reviewPage.js";
-import ReservationCard from "../components/cards/reservationCard.jsx";
 import Home from "../pages/Home.jsx";
-import NavBar from "../components/templates/Navbar.jsx";
+import NavBar from "../components/templates/navBar.jsx";
 import Footer from "../components/templates/Footer.jsx";
 import RestaurantPage from "../pages/RestaurantPage.jsx";
 import ThemeProvider from "../context/ThemeContext";
 import Profile from "../pages/Profile.jsx";
 import ReservationForm from "../pages/ReservationForm.jsx";
-import NotFound from "../components/templates/NotFound.jsx";
+import ProtectedRoute from "./ProtectedRoute";
+import RedirectIfAuthenticated from "./RedirectIfAuthenticated.jsx";
 
 const AppRoutes = () => {
   return (
@@ -22,12 +20,33 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <RedirectIfAuthenticated
+                element={<Login />}
+                redirectTo="/profile"
+              />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RedirectIfAuthenticated
+                element={<Register />}
+                redirectTo="/profile"
+              />
+            }
+          />
           <Route path="/restaurant" element={<RestaurantPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/reservations" element={<ReservationForm />} />{" "}
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="/profile"
+            element={<ProtectedRoute element={<Profile />} />}
+          />
+          <Route
+            path="/reservations"
+            element={<ProtectedRoute element={<ReservationForm />} />}
+          />
         </Routes>
         <Footer />
       </ThemeProvider>
