444dd0c54536b07f6821f1ea77bc71da205d5150
Clara <claraolivam96@gmail.com>
2024-09-02 20:00:13 +0200

    mas cambios

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../src/components/templates/Login";
 import Register from "../src/components/templates/Register";
 import Review from "./components/cards/reviewCard.jsx";
-import ReviewFront from "./pages/reviewPage.js";
 import ReservationCard from "./components/cards/reservationCard.jsx";
 import Home from "./pages/Home.jsx";
+import Navbar from "../src/components/templates/Navbar.jsx";
+import { ThemeProvider } from "../src/context/ThemeContext.js";
 
 
 const App = () => {
   return (
+<<<<<<< Updated upstream
 
     <Router>
       <Routes>
@@ -20,6 +22,20 @@ const App = () => {
       </Routes>
 
     </Router>
+=======
+    <><ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/reservation" element={<ReservationCard />} />
+        </Routes>
+      </Router>
+    </ThemeProvider></>
+>>>>>>> Stashed changes
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import RestaurantList from '../components/templates/RestaurantList'
 
+
 export default function Home() {
   return (
     <div>home<RestaurantList></RestaurantList></div>
